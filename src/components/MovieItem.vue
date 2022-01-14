4ad2872c2f62f3362606e3abfa1b5c44d3a0a099
XanderGale <alessandro.galeazzi@icloud.com>
2022-01-14 04:47:38 +0100

    flags

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -2,7 +2,8 @@
     <div class="main-item">
         <h2>Titolo: {{ movieInfos.title }}</h2>
         <h3>Titolo Originale: {{ movieInfos.original_title }}</h3>
-        <h5>Lingua:</h5> <img :src="require('../assets/img/flags/' + movieInfos.original_language + '.png')" :alt="movieInfo.original_language">
+        <h5>Lingua:</h5> 
+        <img :src="require('../assets/img/flags/' + movieInfos.original_language + '.png')" :alt="movieInfos.original_language + ' flag'">
         <h4>Voto: {{ movieInfos.vote_average }}</h4>
     </div>
 </template>
